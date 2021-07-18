1b3ac0e4dcf64b6722d9e8849605ec63a53e6f12
alvin <jjye81930@gmail.com>
2021-07-18 23:34:20 +0900

    set backend architecture

---
+++ src/main/java/com/toybox/backend/ToyboxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.toybox.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyboxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyboxBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toybox/backend/ToyboxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toybox.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyboxBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
